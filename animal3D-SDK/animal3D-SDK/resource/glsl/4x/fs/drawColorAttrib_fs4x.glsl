c3088d58770f0e27d0d0d84f5b37e8d9fe682371
Jacob <jacob.rose@mymail.champlain.edu>
2020-01-19 17:49:02 -0500

    finished shaders

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/drawColorAttrib_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/drawColorAttrib_fs4x.glsl
@@ -29,9 +29,10 @@
 //	2) assign vertex color to output color
 
 out vec4 rtFragColor;
+varying vec4 varColor;
 
 void main()
 {
 	// DUMMY OUTPUT: all fragments are OPAQUE RED
-	rtFragColor = vec4(1.0, 0.0, 0.0, 1.0);
+	rtFragColor = varColor;
 }
