6bf2efbf54204117a5014628b9fe7b778976448f
Connor Ramsden <connor.ramsden@mymail.champlain.edu>
2020-02-19 20:12:39 -0500

    Finished Part 2: Scene Pass

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/06-deferred/drawLightingData_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/06-deferred/drawLightingData_fs4x.glsl
@@ -30,14 +30,23 @@
 //	3) copy attribute data from varying to respective render targets, 
 //		transforming data accordingly
 
+// (1)
+in vbLightingData{
+	vec4 vViewPosition;
+	vec4 vViewNormal;
+	vec4 vTexcoord;
+	vec4 vBiasedClipCoord;
+};
+
+// (2)
 layout (location = 1) out vec4 rtViewPosition;
 layout (location = 2) out vec4 rtViewNormal;
 layout (location = 3) out vec4 rtAtlasTexcoord;
 
 void main()
 {
-	// DUMMY OUTPUT: all fragments are OPAQUE RED, GREEN AND BLUE
-	rtViewPosition = vec4(1.0, 0.0, 0.0, 1.0);
-	rtViewNormal = vec4(0.0, 1.0, 0.0, 1.0);
-	rtAtlasTexcoord = vec4(0.0, 0.0, 1.0, 1.0);
+	// (3)	
+	rtViewPosition = vec4(vViewPosition.xyz, 1.0);
+	rtViewNormal = normalize(vViewNormal);
+	rtAtlasTexcoord = vec4(vTexcoord.xyz, 1.0);
 }
